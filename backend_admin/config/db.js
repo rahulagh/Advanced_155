2039711d76bc219f60712d35a82714a3dfe74fc8
sairam2381 <sairam2381@gmail.com>
2024-11-08 11:44:59 +0530

    the changes in the frontend are done

--- backend_admin/config/db.js
+++ backend_admin/config/db.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 //this is the connection db that I am having over here
+//hello there this is my db and I will be connecting via this dude
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
